464c3d0278a7899a35bb6fc5c7e39d92b40b8fea
Hiroaki Yutani <yutani.ini@gmail.com>
2017-04-09 18:18:16 +0900

    fix a bug

--- scripts/list_not_installed_packages.R
+++ scripts/list_not_installed_packages.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env r
 
 pkgs <- readLines(file("stdin"))
-pkgs_not_installed <- purrr::keep(pkgs, ~ . %in% installed.packages())
+pkgs_not_installed <- purrr::discard(pkgs, ~ . %in% installed.packages())
 cat(pkgs_not_installed, sep = "\n")
