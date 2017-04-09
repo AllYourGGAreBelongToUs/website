77acb514150cc0dbc76b83ae4a53908b80fad78d
Hiroaki Yutani <yutani.ini@gmail.com>
2017-04-09 15:07:17 +0900

    add commands

--- scripts/generate_Rmd.R
+++ scripts/generate_Rmd.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env r
 
+library(purrr)
+
 doc <- 'Usage: generateRmd.r [-o DIR] PACKAGE
 
 -o --outdir DIR  directory to put Rmds [default: docs]'
@@ -17,8 +19,6 @@ title: |
 rdname: %s
 date: %s
 output: html_document
-layout: article
-category: %s
 images:
 FRONTFOMATTER_IMAGES
 ---
@@ -34,44 +34,75 @@ library(%s)
 footer <- '
 ```'
 
-library(opts$PACKAGE, character.only = TRUE, quietly = TRUE)
-RdDB <- tools::Rd_db(opts$PACKAGE)
+## Functions --------
 
-exported_objnames <- ls(sprintf('package:%s', opts$PACKAGE))
-
-f <- tempfile()
-
-for(Rdname in names(RdDB)) {
-  Rd <- RdDB[[Rdname]]
+load_RdDB <- function(pkg) {
+  library(pkg, character.only = TRUE, quietly = TRUE)
+  RdDB <- tools::Rd_db(pkg)
   
-  # skip if there is no Examples section
-  if(!any(tools:::RdTags(Rd) == "\\examples")) next
+  # discard if there is no Examples section
+  RdDB <- keep(RdDB, ~ any(tools:::RdTags(.) == '\\examples'))
   
   # skip if the Rd is not related to exported objects
-  aliases <- unlist(Rd[which(tools:::RdTags(Rd) %in% c("\\name", "\\alias") )])
-  if(!any(aliases %in% exported_objnames)) next
+  exported_objnames <- ls(sprintf('package:%s', pkg))
+  RdDB <- keep(RdDB, ~ any(get_Rd_aliases(.) %in% exported_objnames))
   
-  title <- paste0(as.character(Rd[[which(tools:::RdTags(Rd) == "\\title")]]), collapse = "")
+  RdDB
+}
+
+extract_Rd_field <- function(Rd, name) {
+  # discard if the Rd field is not 'name' or 'alias'
+  Rd_names <- keep(Rd, ~attr(., 'Rd_tag') == name)
+  
+  # each Rd field is nested; we need to Rd[[1]][[1]] to extract the character.
+  map_chr(Rd_names, `[[`, c(1, 1))
+}
+
+get_Rd_aliases <- function(Rd) {
+  c(
+    extract_Rd_field(Rd, '\\name'),
+    extract_Rd_field(Rd, '\\alias')
+  )
+}
+
+get_Rd_title <- function(Rd) {
+  title <- extract_Rd_field(Rd, '\\title')
+  title <- paste0(title, collapse = '')
   title <- gsub('\n', '\n  ', title)
   title <- gsub('list\\("(\\w+)"\\)', '\\1', title)
   
-  rdname <- sub("\\.Rd$", "", Rdname)
+  title
+}
+
+write_Rmd <- function(Rd, outdir) {
+  title <- get_Rd_title(Rd)
+  rdname <- extract_Rd_field(Rd, '\\name')
   
-  # use the date when Zero Wing released on Sega Mega Drive! 
-  date  <- "1991-05-31"
+  out <- file.path(outdir, paste0(rdname, ".Rmd"))
   
-  out <- file.path("_source", sprintf("%s-%s.Rmd", date, rdname))
-  
-  # header (Note that categories are normalized to dwoncase by Jekyll anyway)
-  cat(sprintf(header_tmpl, title, rdname, strftime(Sys.time(), "%Y-%m-%d"), tolower(opts$PACKAGE), opts$PACKAGE, rdname), file = out)
+  # header
+  cat(sprintf(header_tmpl,
+              title,
+              rdname,
+              strftime(Sys.time(), '%Y-%m-%d'),
+              pkg,
+              rdname),
+      file = out)
   
   # codes
-  tools::Rd2ex(Rd, f)
-  
+  tmp_ex <- tempfile()
+  tools::Rd2ex(Rd, tmp_ex)
+  ex <- readLines(tmp_ex)
   # dischard first six lines
-  ex <- readLines(f)
-  cat(paste0(ex[7:length(ex)], collapse = "\n"), file = out, append = TRUE)
+  cat(paste0(ex[7:length(ex)], collapse = '\n'), file = out, append = TRUE)
   
   # footer
   cat(sprintf(footer, title), file = out, append = TRUE)
+}
+
+
+## Main --------
+
+for(Rd in load_RdDB(pkg)) {
+  write_Rmd(Rd, outdir)
 }
